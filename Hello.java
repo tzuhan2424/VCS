f94064551649465c68b4fef9232c36e603b30d07
tzuhan2424 <102434871+tzuhan2424@users.noreply.github.com>
2022-03-26 16:14:52 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello{
+    private String greeting = "How are you";
+    public Hello(){
+
+    }
+    
+    public void main(Stirng[] args){
+        System.out.println("Hello");
+    }
+    
+    public static void status(){
+        System.out.println("status: good");
+    }
+}
+
+public class Thank_you extends Hello{
+    
+}
